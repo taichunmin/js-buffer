dd5738c0fd30025ac02b3ebd0e3992c97d443509
taichunmin <taichunmin@gmail.com>
2024-04-16 11:36:29 +0800

    v0.1.0

---
+++ .eslintrc.js
@@ -0,0 +1,30 @@
+module.exports = {
+  env: {
+    es2022: true,
+    jest: true,
+  },
+  extends: ['love'],
+  rules: {
+    '@typescript-eslint/no-unsafe-argument': 0, // 0 = off, 1 = warn, 2 = error
+    '@typescript-eslint/non-nullable-type-assertion-style': 0, // 0 = off, 1 = warn, 2 = error
+    '@typescript-eslint/return-await': 0, // 0 = off, 1 = warn, 2 = error
+    '@typescript-eslint/strict-boolean-expressions': 0, // 0 = off, 1 = warn, 2 = error
+    '@typescript-eslint/unbound-method': 0, // 0 = off, 1 = warn, 2 = error
+    'multiline-ternary': 0, // 0 = off, 1 = warn, 2 = error
+    'no-return-await': 0, // 0 = off, 1 = warn, 2 = error
+    'comma-dangle': ['error', {
+      arrays: 'always-multiline',
+      objects: 'always-multiline',
+      imports: 'always-multiline',
+      exports: 'always-multiline',
+      functions: 'only-multiline',
+    }],
+    '@typescript-eslint/comma-dangle': ['error', {
+      arrays: 'always-multiline',
+      objects: 'always-multiline',
+      imports: 'always-multiline',
+      exports: 'always-multiline',
+      functions: 'only-multiline',
+    }],
+  },
+}
---
+++ tsup.config.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from 'tsup'
+
+export default defineConfig({
+  clean: true,
+  entry: ['lib/buffer.ts'],
+  dts: true,
+  format: ['cjs', 'esm', 'iife'],
+  minify: true,
+  sourcemap: true,
+  splitting: false
+})
